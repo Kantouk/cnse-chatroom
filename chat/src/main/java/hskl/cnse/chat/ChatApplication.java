e6a9a2ba2001b8c6d68a069603a5aba2a87a41fc
argi0001 <argi0001@stud.hs-kl.de>
2023-12-11 13:52:11 +0100

    Google Login hinzugefügt
    Startseite: localhost:8080
    Login: localhost:8080/login
    Nur mit eingeloggtem Zugang: localhost:8080/secured

--- chat/src/main/java/hskl/cnse/chat/ChatApplication.java
+++ chat/src/main/java/hskl/cnse/chat/ChatApplication.java
@@ -1,10 +1,23 @@
 package hskl.cnse.chat;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchange.Principal;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ChatApplication {
+
+	 @GetMapping
+    public String welcome() {
+        return "Welcome to Google !!";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("username : " + principal.getName());
+        return principal;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
