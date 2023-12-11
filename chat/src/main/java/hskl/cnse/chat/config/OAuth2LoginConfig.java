52c038fbdd3b98fff349da399673a3bbda33cf17
argi0001 <argi0001@stud.hs-kl.de>
2023-12-11 15:14:28 +0100

    Update OAuth2LoginConfig.java

--- chat/src/main/java/hskl/cnse/chat/config/OAuth2LoginConfig.java
+++ chat/src/main/java/hskl/cnse/chat/config/OAuth2LoginConfig.java
@@ -1,22 +1,16 @@
 package hskl.cnse.chat.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 
 @Configuration
 public class OAuth2LoginConfig {
-
+ /* 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
         return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
     }
 
-    private ClientRegistration googleClientRegistration() {
+   private ClientRegistration googleClientRegistration() {
     return ClientRegistration.withRegistrationId("google")
         .clientId("Ihr-Google-Client-ID")
         .clientSecret("Ihr-Google-Client-Geheimnis")
@@ -30,5 +24,5 @@ public class OAuth2LoginConfig {
         .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE) // Setzen Sie den authorizationGrantType
         .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}") // Setzen Sie die redirectUri
         .build();
-}
+}*/
 }
