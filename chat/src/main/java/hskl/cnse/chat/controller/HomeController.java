dca4d94fb023a9d6d40a7dd20fc0c210841c8a46
Kantouk <jensmueller.vxln@gmail.com>
2023-12-12 11:26:02 +0100

    .

--- chat/src/main/java/hskl/cnse/chat/controller/HomeController.java
+++ chat/src/main/java/hskl/cnse/chat/controller/HomeController.java
@@ -1,9 +1,10 @@
 package hskl.cnse.chat.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     //@GetMapping("/")
@@ -13,6 +14,6 @@ public class HomeController {
 
     @GetMapping("/secured")
     public String secured() {
-        return "Hello, Secured!";
+        return "chat";
     }
 }
